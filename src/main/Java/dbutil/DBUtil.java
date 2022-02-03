238d1b974017954f036076b4e271dbf7cf07e8da
Minria <810760487@qq.com>
2022-02-03 12:20:52 +0800

    数据库连接断开管理

---
+++ src/main/Java/dbutil/DBUtil.java
@@ -0,0 +1,55 @@
+package dbutil;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class DBUtil {
+    private static final String URL="jdbc:mysql://127.0.0.1:3306/musicserver?characterEncoding=utf-8&&useSSL=true";
+    private static final String USER="root";
+    private static final String PASSWORD="qwer1234";
+    private static volatile DataSource dataSource=null;
+    private static DataSource getDataSource() {
+        if (dataSource == null) {
+            synchronized (DBUtil.class) {
+                if (dataSource == null) {
+                    dataSource = new MysqlDataSource();
+                    // 还需要给 DataSource 设置一些属性.
+                    ((MysqlDataSource)dataSource).setURL(URL);
+                    ((MysqlDataSource)dataSource).setUser(USER);
+                    ((MysqlDataSource)dataSource).setPassword(PASSWORD);
+                }
+            }
+        }
+        return dataSource;
+    }
+    public static Connection getConnection() {
+        try {
+            return getDataSource().getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static void close(Connection connection, PreparedStatement statement, ResultSet resultSet){
+        try {
+            if(resultSet!=null){
+                resultSet.close();
+            }
+            if(statement!=null){
+                statement.close();
+            }
+            if(connection!=null){
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
