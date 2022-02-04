1a93fd80c4375f8998d780e6044f7532009a5ac7
Minria <810760487@qq.com>
2022-02-04 12:40:16 +0800

    删除音乐

--- src/main/Java/servlet/music/DeleteMusicServlet.java
+++ src/main/Java/servlet/music/DeleteMusicServlet.java
@@ -3,6 +3,8 @@ package servlet.music;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dao.MusicDao;
 import entity.Music;
+import service.MusicService;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,27 +23,8 @@ public class DeleteMusicServlet extends HttpServlet {
         String strId=req.getParameter("id");
         int id=Integer.parseInt(strId);
         System.out.println("删除id->"+id);
-        Music music= MusicDao.findMusicById(id);
-        if(music==null){
-            return;
-        }else{
-            boolean successful=MusicDao.deleteMusic(id);
-            if(successful){
-                String path="D:\\Github\\OnlineMusic\\target\\OnlineMusic\\"+music.getUrl()+".mp3";
-                System.out.println("音乐路径->"+path);
-                File file = new File(path);
-                System.out.println("文件是否存在->"+file.exists());
-                if(file.delete()){
-                    map.put("msg",true);
-                }else{
-                    map.put("msg",false);
-                    System.out.println("文件名："+file.getName());
-                    System.out.println("删除文件失败！");
-                }
-            }else{
-                map.put("msg",false);
-            }
-        }
+        boolean ret= MusicService.deleteMusic(id);
+        map.put("msg",ret);
         //将map转化为json
         ObjectMapper mapper=new ObjectMapper();
         mapper.writeValue(resp.getWriter(),map);
