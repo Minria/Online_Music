5d646fcdfc32f433ed1fa03e7b5b9f3fd1f2be2c
Minria <810760487@qq.com>
2022-02-04 12:39:52 +0800

    音乐服务层

--- src/main/Java/service/MusicService.java
+++ src/main/Java/service/MusicService.java
@@ -1,4 +1,32 @@
 package service;
 
+import dao.MusicDao;
+import entity.Music;
+
+import java.io.File;
+
 public class MusicService {
+    public static boolean deleteMusic(int id){
+        Music music= MusicDao.findMusicById(id);
+        if(music==null){
+            return false;
+        }else{
+            boolean successful=MusicDao.deleteMusic(id);
+            if(successful){
+                String path="D:\\Github\\OnlineMusic\\target\\OnlineMusic\\"+music.getUrl()+".mp3";
+                System.out.println("音乐路径->"+path);
+                File file = new File(path);
+                System.out.println("文件是否存在->"+file.exists());
+                if(file.delete()){
+                    return true;
+                }else{
+                    System.out.println("文件名："+file.getName());
+                    System.out.println("删除文件失败！");
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+    }
 }
